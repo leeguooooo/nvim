6e68042823d63e62093eda08cecf54d83a868c5b
guoli <guoli@zhihu.com>
2022-07-14 00:35:21 +0800

    init

---
+++ init.vim
@@ -0,0 +1,7 @@
+" Packer插件管理
+lua require('plugins')
+" 皮肤设置
+set background=dark
+colorscheme gruvbox
+" 插件配置
+lua require('plugin-config/nvim-tree')
---
+++ lua/plugins.lua
@@ -0,0 +1,14 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  -- gruvbox theme
+    use {
+        "ellisonleao/gruvbox.nvim",
+        requires = {"rktjmp/lush.nvim"}
+    }
+    -- nvim-tree (新增)
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons'
+    }
+end)
